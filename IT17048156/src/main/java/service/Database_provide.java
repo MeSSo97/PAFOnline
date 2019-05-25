35307eaa9acd95f9625499bc57f2c54b2884446c
MeSSo97 <sandu.sandupama.ss@gmail.com>
2019-05-25 23:21:40 +0530

    FirstCommit

---
+++ IT17048156/src/main/java/service/Database_provide.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Database_provide {
+	public interface Provider {
+		String DRIVER="com.mysql.jdbc.Driver";  
+		String CONNECTION_URL="jdbc:mysql://localhost:3306/lankan";  
+		String USERNAME="root";  
+		String PASSWORD="root";  
+	}
+
+}
